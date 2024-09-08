78aa65f05b020a0716e34920bf8bbb7d05da31d2
Endorpheen <butilenkov@mail.ru>
2024-09-08 22:20:40 +0300

    Add Loading component and styles

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner"></div>
+      <p>Загрузка...</p>
+    </div>
+  );
+};
+
+export default Loading;
