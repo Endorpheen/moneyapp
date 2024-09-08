756ec9446f81c73de242acd7c3ba762f20d933e3
Endorpheen <butilenkov@mail.ru>
2024-09-08 23:22:44 +0300

    Successful test of production build on localhost:41709

--- moneyapp/settings.py
+++ moneyapp/settings.py
@@ -173,7 +173,7 @@ SIMPLE_JWT = {
 }
 
 # Настройки CORS
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
@@ -185,6 +185,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
     "http://127.0.0.1:3002",
     "http://127.0.0.1:3003",
+    "http://localhost:41709",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -192,6 +193,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3002",
     "http://127.0.0.1:3002",
+    "http://localhost:41709",
 ]
 
 CORS_ALLOW_METHODS = [
